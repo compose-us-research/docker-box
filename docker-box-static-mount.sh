4c916f9e22e890abc47054ce6c2e5e5f03167c5b
Joern Bernhardt <joern.bernhardt@compose.us>
2020-01-05 01:31:43 +0100

    Rename to docker-box and properly quote arguments for docker
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- docker-box-static-mount.sh
+++ docker-box-static-mount.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 APP_TO_RUN="$1"
-shift
-ARGUMENTS="$@"
 SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P )"
 
-APP_PATH="${SCRIPT_PATH}/apps/${APP_TO_RUN}"
-VOLUME_DIR="${SCRIPT_PATH}/sandbox"
-DOCKERFILE="${APP_PATH}/Dockerfile"
+if [ -z ${DOCKER_BOX_APPS_PATH+x} ]; then
+  DOCKER_BOX_APPS_PATH="${SCRIPT_PATH}/apps"
+fi
 
+APP_PATH="${DOCKER_BOX_APPS_PATH}/${APP_TO_RUN}"
+DOCKERFILE="${APP_PATH}/Dockerfile"
 DOCKERFILE_VERSION=$(md5 -q "$DOCKERFILE")
 IMAGE_NAME="${APP_TO_RUN}:${DOCKERFILE_VERSION}"
+VOLUME_DIR="${SCRIPT_PATH}/sandbox"
 
 # If the docker image does not exist yet, create it
 if ! docker inspect "$IMAGE_NAME" > /dev/null; then
@@ -18,10 +19,20 @@ if ! docker inspect "$IMAGE_NAME" > /dev/null; then
   docker build -t "$IMAGE_NAME" -f "$DOCKERFILE" $APP_PATH
 fi
 
+# Concatenate all remaining arguments and put them into quotes
+shift
+arguments=""
+for i in "$@"; do
+  arguments="$arguments"'"'"$i"'" '
+done
+echo "$arguments"
+
 echo "Running $APP_TO_RUN"
-docker run \
+
+# Run the docker command with properly quoted arguments
+echo docker run \
        --rm \
        --name "$APP_TO_RUN" \
        --volume "${VOLUME_DIR}:/app" \
        "$IMAGE_NAME" \
-       "$APP_TO_RUN" "$ARGUMENTS"
+       "$APP_TO_RUN" "$arguments" | sh
--- docker-box.sh
+++ docker-box.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env bash
 
 APP_TO_RUN="$1"
-shift
-ARGUMENTS="$@"
 SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P )"
 
-APP_PATH="${SCRIPT_PATH}/apps/${APP_TO_RUN}"
+if [ -z ${DOCKER_BOX_APPS_PATH+x} ]; then
+  DOCKER_BOX_APPS_PATH="${SCRIPT_PATH}/apps"
+fi
+
+APP_PATH="${DOCKER_BOX_APPS_PATH}/${APP_TO_RUN}"
 DOCKERFILE="${APP_PATH}/Dockerfile"
 DOCKERFILE_VERSION=$(md5 -q "$DOCKERFILE")
 IMAGE_NAME="${APP_TO_RUN}:${DOCKERFILE_VERSION}"
 VOLUME_DIR="$(pwd)"
 
+# If the docker image does not exist yet, create it
 if ! docker inspect "$IMAGE_NAME" > /dev/null; then
   echo "Building $IMAGE_NAME"
   docker build -t "$IMAGE_NAME" -f "$DOCKERFILE" $APP_PATH
 fi
 
+# Concatenate all remaining arguments and put them into quotes
+shift
+arguments=""
+for i in "$@"; do
+  arguments="$arguments"'"'"$i"'" '
+done
+echo "$arguments"
+
 echo "Running $APP_TO_RUN"
-docker run \
+
+# Run the docker command with properly quoted arguments
+echo docker run \
        --rm \
        --name "$APP_TO_RUN" \
        --volume "${VOLUME_DIR}:/app" \
        "$IMAGE_NAME" \
-       "$APP_TO_RUN" "$ARGUMENTS"
+       "$APP_TO_RUN" "$arguments" | sh
