058ae5313f36d06fcf3531b982f1051a8a798937
Joern Bernhardt <joern.bernhardt@compose.us>
2020-01-22 20:39:20 +0100

    Remove port settings in favor or custom extra options
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- docker-box-run.sh
+++ docker-box-run.sh
@@ -23,20 +23,16 @@ done
 
 echo "Running $APP_TO_RUN"
 
-PORT_SETTING=""
-if [ ! -z "$PORT" ]; then
-  PORT_SETTING="-p ${PORT}:${PORT}"
-fi
-
 # Run the docker command with properly quoted arguments
 COMMAND="""docker run \
        --rm \
        --name "$APP_TO_RUN" \
-       ${PORT_SETTING} \
+       ${EXTRA_OPTIONS} \
        --volume "${VOLUME_DIR}:/app" \
        -it \
        "$IMAGE_NAME" \
        "$arguments"
 """
 
+echo "$COMMAND"
 bash -c "$COMMAND"
