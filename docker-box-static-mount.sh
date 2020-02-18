1564e29f8ddd8be466821ba91091d5f02e9657af
Joern Bernhardt <joern.bernhardt@compose.us>
2020-02-18 21:49:49 +0100

    Remove unnecessary docker compose hash for apps without docker-compose.yml
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- docker-box-static-mount.sh
+++ docker-box-static-mount.sh
@@ -32,7 +32,7 @@ else
 
   DOCKERFILE="${APP_PATH}/Dockerfile"
   DOCKERFILE_HASH=$(md5 -q "${DOCKERFILE}")
-  DOCKER_BUILD_VERSION=$(echo "${DOCKERFILE_HASH}${DOCKER_COMPOSE_HASH}" | md5)
+  DOCKER_BUILD_VERSION="${DOCKERFILE_HASH}"
   IMAGE_NAME="${APP_TO_RUN}:${DOCKER_BUILD_VERSION}"
   # If the docker image does not exist yet, create it
   if ! docker inspect "$IMAGE_NAME" > /dev/null; then
